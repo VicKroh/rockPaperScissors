47a15df6c6fdc5cbffe6aae6c4afe22a3d75bc10
Victor Krohmer <victor.krohmer@gmx.de>
2022-02-08 15:24:35 +0100

    First running version

--- rock_paper_scissors/src/App.tsx
+++ rock_paper_scissors/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ComponentState } from 'react';
 
 import Scissors from './scissors.png';
 import Rock from './rock.png';
@@ -6,40 +6,39 @@ import Paper from './paper.png';
 import White from './white.png';
 
 import './App.css';
-import { LookupFunction } from 'net';
 
 let winner: number = -1;
 
 
 
-const lookUpWinner = {
-  "paper": "scissors",
-  "scissors": "Rock",
-  "rock": "Paper"
-}
-
-const lookUpChoices = [
-  { id: 0, name: "White", icon: White },
-  { id: 1, name: "Rock", icon: Rock },
-  { id: 2, name: "Paper", icon: Paper },
-  { id: 3, name: "scissors", icon: Scissors }
-
+const lookUpGameState = [
+  "You Win!",
+  "You Lose :(",
+  "Draw"
 ]
 
-function calculateWinner(player1: string): any {
-
-
-  return 0;
+interface choiceObject {
+  id: number, name: string, icon: any, losesTo: number
 }
 
-function displayChoices(player1: string, player2: string) {
-  <div>
-    <img src={Scissors} className="player1Choice" alt="logo" />
-    <img src={Scissors} className="player2Choice" alt="logo" />
-  </div>
+
+const lookUpChoices = [
+  { id: 0, name: "Rock", icon: Rock, losesTo: 1 },
+  { id: 1, name: "Paper", icon: Paper, losesTo: 2 },
+  { id: 2, name: "scissors", icon: Scissors, losesTo: 0 },
+
+  { id: 3, name: "White", icon: White, losesTo: null } // TODO workaround?
+]
+
+
+function displayChoices(player1: any, player2: any): ComponentState {
+  return (<div>
+    <img src={player1.icon} className="player1Choice" alt="logo" />
+    <img src={player2.icon} className="player2Choice" alt="logo" />
+  </div>)
 }
 
-function displayWinner() {
+/* function displayWinner() {
   if (winner > 0) {
     return "Choose a Weapon above!";
   } else if (winner === 0) {
@@ -49,34 +48,67 @@ function displayWinner() {
   } else {
     return "Bot wins!"
   }
+} */
+
+function calculateWinner(playerChoice: number, computerChoice: number): number {
+
+  return 0;
 }
 
 
-
 function App() {
-  const [player1, setPlayer1] = useState(lookUpChoices.find(input => input.id == 0));
-  const [computer, setComputer] = useState(lookUpChoices.find(input => input.id == 0));
+  const [player1, setPlayer1] = useState(lookUpChoices.find(input => input.id === 3));
+  const [computer, setComputer] = useState(lookUpChoices.find(input => input.id === 3));
 
-  const [gameState, setGameState] = useState(null);
-
-
+  const [gameState, setGameState]: any = useState();
 
   useEffect(() => {
-    const computerchoice: any = lookUpChoices[Math.floor(Math.random() * lookUpChoices.length)];
-    setComputer(computerchoice);
-  })
 
+  })
 
   function handleClick(choice: number) {
 
-    const userChoice: any = lookUpChoices.find(input => input.id == choice);
+        
+    const computerchoice: any = lookUpChoices[Math.floor(Math.random() * (lookUpChoices.length - 1))]; // -1 to prevent choosing nothing
+
+    setComputer(computerchoice);
+    console.log("computer: " + computerchoice.id)
+
+
+
+    if (choice === lookUpChoices.length - 1) {
+      choice = Math.floor(Math.random() * (lookUpChoices.length - 1))
+    }
+
+    const userChoice: any = lookUpChoices.find(input => input.id === choice);
     setPlayer1(userChoice);
+    console.log("player: " + userChoice.id)
+
+
+
+
+    if (userChoice.id === computerchoice.id) {
+      setGameState("draw")
+      console.log("wincondition: " + computerchoice.id + " " + userChoice.id + " = Draw")
+
+    } else if (userChoice.losesTo === computerchoice.id) {
+      console.log("wincondition: " + userChoice.id + " " + computerchoice.id + " = lose")
+
+      setGameState("You Lose")
+    } else if (computerchoice.losesTo === userChoice.id) {
+      console.log("wincondition: " + computerchoice.id + " " + userChoice.id + " = win")
+
+      setGameState("You Win")
+    }
   }
 
-  function renderIcons(choice: any) {
-    const Component = choice.icon;
-    return <Component />
-  }
+  /* 
+    function renderIcons(choice: any) {
+      const Component = choice.icon;
+      return <Component />
+    } */
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -86,20 +118,17 @@ function App() {
           Pick your weapon:
         </p>
         <div>
-          <button className="buttons" onClick={() => (handleClick(1))}>Rock</button>
-          <button className="buttons" onClick={() => (handleClick(2))}>Paper</button>
-          <button className="buttons" onClick={() => (handleClick(3))}>Scissors</button>
-          <button className="buttons" onClick={() => (handleClick(4))}>Random</button>
+          <button className="buttons" onClick={() => (handleClick(0))}>Rock</button>
+          <button className="buttons" onClick={() => (handleClick(1))}>Paper</button>
+          <button className="buttons" onClick={() => (handleClick(2))}>Scissors</button>
+          <button className="buttons" onClick={() => (handleClick(3))}>Random</button>
         </div>
 
-    
-        <div>
-          <img src={White} className="player1Choice" alt="logo" />
-          <img src={White} className="player2Choice" alt="logo" />
-        </div>
+        {displayChoices(player1, computer)}
+
+        {gameState}
 
 
-        <div> {() => displayWinner()} </div>
 
       </header>
     </div>
